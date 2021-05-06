29e9ba07616408105413a9272ed6f335ae7026e2
nimiko24 <68658629+nimiko24@users.noreply.github.com>
2021-05-05 22:29:54 -0500

    End of error

--- Practice/Third-Chapter.cpp
+++ Practice/Third-Chapter.cpp
@@ -10,22 +10,10 @@ using std::string;
 
 string my_name;
 
-string courses;
-
 class MyFriends {
 public: 
-    void my_function() {
-        cout << "Hello " << my_name << endl;
-    }
     string eyes;
     int age;    
-};
-
-class Homework {
-public:
-    void courses() {
-        cout << "Your courses are: " << courses << endl;
-    }
 };
 
 void my_function() {
@@ -35,6 +23,8 @@ void my_function() {
 int main() {
     cout << "Your name is: " << endl;
     cin >> my_name;
+    
+    my_function();
 
     std::transform(my_name.begin(), my_name.end(), my_name.begin(), ::tolower);
 
@@ -47,13 +37,11 @@ int main() {
     sandra.age = 0x12;
 
     if (my_name == "sandra" || my_name == "sand") { //I need a break!
-        sandra.my_function();
         cout << sandra.eyes << endl;
         cout << sandra.age << endl;
     }
 
     else if (my_name == "nico") {
-        nico.my_function();
         cout << nico.eyes << endl;
         cout << nico.age << endl;
     }
@@ -61,10 +49,6 @@ int main() {
     else {
         cout << "You are a stranger!" << endl;
     }
-
-    cout << "Your courses are: " << endl;
-    getline( cin, courses );
-
     
     return 0;
 }
